bfa7462abb80a54766bcffb398cba1b7d94c7919
Miran Seo <smiran3@hanmail.net>
2015-01-30 04:27:21 +0900

    Glide 적용

--- app/src/main/java/com/example/miranseo/imagetest/ImageAdapter.java
+++ app/src/main/java/com/example/miranseo/imagetest/ImageAdapter.java
@@ -1,3 +1,103 @@
+//package com.example.miranseo.imagetest;
+//
+//import android.content.Context;
+//import android.net.Uri;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.BaseAdapter;
+//import android.widget.GridView;
+//import android.widget.ImageView;
+//import com.example.miranseo.imagetest.Images;
+//import com.bumptech.glide.Glide;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import view.ViewHolder;
+//
+///**
+// * Created by Miran Seo on 15. 1. 21.
+// */
+//public class ImageAdapter extends ArrayAdapter<String> {
+////    private Images images;
+////    private final LayoutInflater mInflater;
+////    private int mResource;
+////
+////    public ImageAdapter(Context context, int resource) {
+////        super(context, resource);
+////        mResource = resource;
+////        mInflater = LayoutInflater.from(context);
+////    }
+////
+//////    public ImageAdapter(Context context, int resource, ArrayList<Images.Image> images) {
+//////        super(context, resource);
+//////        mResource = resource;
+//////        mInflater = LayoutInflater.from(context);
+//////    }
+////
+////    public void setPhoto(Images images){
+////
+////        this.images = images;
+////    }
+//private final LayoutInflater mInflater;
+//    private int mResource;
+//
+//    public ImageAdapter(Context context, int resource) {
+//        super(context, resource);
+//        mResource = resource;
+//        mInflater = LayoutInflater.from(context);
+//    }
+//
+//
+//    // create a new ImageView for each item referenced by the Adapter
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            convertView = mInflater.inflate(mResource, parent, false);
+//        }
+//
+//        ImageView imageView = ViewHolder.get(convertView, R.id.image);
+////
+////
+////
+////
+////        ImageView imageView;
+////        if (convertView == null) {           // if it's not recycled, initialize some attributes
+////            imageView = new ImageView(mContext);
+////            imageView.setLayoutParams(new GridView.LayoutParams(250,250)); // view에 대한 높이와 너비
+////            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP); // 필요하다면 이미지를 가운데쪽으로 잘라냄.
+////            imageView.setPadding(0, 0, 0, 0); //모든 면을 위한 패딩을 정의
+////        } else {
+////            imageView = (ImageView) convertView;
+////        }
+//        String url = getItem(position);
+//        Glide.with(getContext())
+//
+//                        .fromString()
+//                        .load(url)
+//                        //adapter에 이미 순서대로 내용 들어가있으므로 해당 위치에 해당하는 것들을 불러와야함.
+//                        .fitCenter()
+//                        .error(R.drawable.ic_launcher)
+//                        .thumbnail(0.1f)
+//                        .into(imageView);
+//        //imageView.setImageURI(Uri.parse(imageList.get(position).getPhoto()));
+//        //notifyDataSetChanged();
+//
+//
+//
+//        return imageView;
+//    }
+//
+//    // references to our images
+//   // public ArrayList<Images> mThumbIds = new ArrayList<Images>();
+// //   public static ArrayList mThumbIds = new ArrayList();
+//
+//    //public String mThumbIds[] = null;
+//
+//}
+
 package com.example.miranseo.imagetest;
 
 import android.content.Context;
@@ -9,10 +109,13 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by Miran Seo on 15. 1. 21.
@@ -51,14 +154,20 @@ public class ImageAdapter extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
-//        Glide.with(mContext)
-//                .fromString()
-//                .load(imageList.get(position).getPhoto())
-//                        //adapter에 이미 순서대로 내용 들어가있으므로 해당 위치에 해당하는 것들을 불러와야함.
-//                .fitCenter()
-//                .error(R.drawable.ic_launcher)
-//                .into(imageView);
-        imageView.setImageURI(Uri.parse(imageList.get(position).getPhoto()));
+
+        //String url = getItem(position).toString();
+        String url = imageList.get(position).getPhoto();
+        Glide.with(mContext)
+
+                        .fromString()
+                        .load(url)
+                        //adapter에 이미 순서대로 내용 들어가있으므로 해당 위치에 해당하는 것들을 불러와야함.
+                        .fitCenter()
+                        .error(R.drawable.ic_launcher)
+                        .thumbnail(0.1f)
+                        .into(imageView);
+
+        //imageView.setImageURI(Uri.parse(imageList.get(position).getPhoto()));
         notifyDataSetChanged();
 
 
@@ -67,7 +176,7 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     // references to our images
-   // public ArrayList<Images> mThumbIds = new ArrayList<Images>();
+    // public ArrayList<Images> mThumbIds = new ArrayList<Images>();
     public static ArrayList mThumbIds = new ArrayList();
 
     //public String mThumbIds[] = null;
